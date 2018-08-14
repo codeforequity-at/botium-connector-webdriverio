e3a6f1a257cae816924e31ff0abf7559ee3db08e
treml <ftreml@gmail.com>
2018-08-14 17:52:58 +0200

    Firefox + Edge Support

--- profiles/messenger_com.js
+++ profiles/messenger_com.js
@@ -7,8 +7,8 @@ module.exports = {
       .waitForVisible('#pass', 20000)
       .waitForVisible('#loginbutton', 20000)
       .then(() => debug('facebook login form fully visible'))
-      .setValue('#email', container.caps['WEBDRIVERIO_USERNAME'] || process.env['BOTIUM_WEBDRIVERIO_USERNAME'])
-      .setValue('#pass', container.caps['WEBDRIVERIO_PASSWORD'] || process.env['BOTIUM_WEBDRIVERIO_PASSWORD'])
+      .setValue('#email', container.caps['WEBDRIVERIO_USERNAME'])
+      .setValue('#pass', container.caps['WEBDRIVERIO_PASSWORD'])
       .click('#loginbutton')
       .waitForVisible('._4_j4', 20000)
       .waitForVisible('div._o46', 20000)
@@ -24,10 +24,15 @@ module.exports = {
       }
     }
     if (msg.messageText) {
-      return browser
-        .keys(msg.messageText)
-        .keys('Enter')
-        .pause(1000)
+      return browser.elementActive().then((r) => {
+        const activeElement = r.value && (r.value.ELEMENT || r.value["element-6066-11e4-a52e-4f735466cecf"])
+        if(activeElement){
+          return browser
+            .elementIdValue(activeElement, msg.messageText)
+            .elementIdValue(activeElement, 'Enter')
+            .pause(1000)
+        }
+      })
     }
   },
   'WEBDRIVERIO_OUTPUT_ELEMENT': 'div._o46:not(._3i_m)',
