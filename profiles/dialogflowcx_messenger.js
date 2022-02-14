b8bc26334fd071ff2c2d5ec4a284fbec3a527ae2
Florian Treml <florian.treml@botium.at>
2022-02-14 13:34:34 +0100

    DialogflowCX Map df-chips as extra buttons

--- profiles/dialogflowcx_messenger.js
+++ profiles/dialogflowcx_messenger.js
@@ -1,6 +1,7 @@
 module.exports = {
   WEBDRIVERIO_INPUT_NAVIGATION_BUTTONS: ['>>>.bot-animation'],
   WEBDRIVERIO_INPUT_ELEMENT: '>>>.input-box-wrapper input',
-  WEBDRIVERIO_OUTPUT_ELEMENT: '>>>.bot-message:not(#typing), df-card, df-chips',
-  WEBDRIVERIO_OUTPUT_ELEMENT_BUTTONS: '>>>df-button a, .df-chips-wrapper a'
+  WEBDRIVERIO_OUTPUT_ELEMENT: '>>>.bot-message:not(#typing), df-card',
+  WEBDRIVERIO_OUTPUT_ELEMENT_BUTTONS: '>>>df-button a',
+  WEBDRIVERIO_OUTPUT_ELEMENT_EXTRA_BUTTONS: '>>>.df-chips-wrapper:not(.clicked) a'
 }
